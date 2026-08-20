4f80bc0ba429a315d5746ef780719b1819df8da9
khushipatel3411 <pkhushik1709@gmail.com>
2026-08-20 14:06:56 +0530

    prisma

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -10,6 +10,4 @@ export const prisma =
     log: ["query", "error", "warn"],
   });
 
-if (process.env.NODE_ENV !== "production") {
-  globalForPrisma.prisma = prisma;
-}
+globalForPrisma.prisma = prisma;
